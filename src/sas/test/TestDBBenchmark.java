f31a30ea0550e38a098bb14660de8d89f3b7685e
dwing <dwing4g@gmail.com>
2013-12-07 18:39:58 +0800

    improve benchmark

--- src/sas/test/TestDBBenchmark.java
+++ src/sas/test/TestDBBenchmark.java
@@ -7,8 +7,10 @@ import sas.core.Procedure;
 import sas.core.Storage;
 import sas.core.StorageMVStore;
 import sas.core.StorageMapDB;
+import sas.core.Util;
 
 // JVM: -Xms512M -Xmx512M
+// RUN: start.bat b md 8 0 150000
 public final class TestDBBenchmark
 {
 	public static void main(String[] args) throws Throwable
@@ -16,10 +18,10 @@ public final class TestDBBenchmark
 		Storage sto = StorageMapDB.instance();
 		if(args.length > 0)
 		{
-			if("mv1".equals(args[0]))
+			if("mv".equals(args[0]))
 				sto = StorageMVStore.instance();
 			else
-				sto = StorageMapDB.instance(); // if("md1".equals(args[0]))
+				sto = StorageMapDB.instance(); // if("md".equals(args[0]))
 		}
 		int count = 8;
 		if(args.length > 1)
@@ -29,6 +31,8 @@ public final class TestDBBenchmark
 			else
 				count = Integer.parseInt(args[1]);
 		}
+		final int from = (args.length > 2 ? Integer.parseInt(args[2]) : 0);
+		final int keys = (args.length > 3 ? Integer.parseInt(args[3]) : 150000);
 
 		System.out.println("begin --- " + sto.getClass().getName() + ' ' + count);
 		DBManager.instance().startup(sto);
@@ -44,7 +48,8 @@ public final class TestDBBenchmark
 			t = System.currentTimeMillis();
 			for(int i = 0; i < n; ++i)
 			{
-				final long index = i;
+				final long index1 = from + (Util.getRand().nextInt() & 0x7fffffff) % keys; // i;
+				final long index2 = from + (Util.getRand().nextInt() & 0x7fffffff) % keys; // n - i - 1;
 				final long t0 = System.currentTimeMillis();
 				new Procedure()
 				{
@@ -54,24 +59,24 @@ public final class TestDBBenchmark
 						long t1 = System.currentTimeMillis();
 						long tt = t1 - t0;
 						if(tt >= 250) System.out.println("--- proc delay=" + tt);
-						lock(AllTables.TestTable.lockid(index),
-						        AllTables.TestTable.lockid(n - index - 1));
-						TestType a = AllTables.TestTable.get(index);
-						TestType b = AllTables.TestTable.get(n - index - 1);
+						lock(AllTables.TestTable.lockid(index1),
+						        AllTables.TestTable.lockid(index2));
+						TestType a = AllTables.TestTable.get(index1);
+						TestType b = AllTables.TestTable.get(index2);
 						if(a == null)
 						{
 							a = new TestType();
-							a.v4 = (int)index;
-							AllTables.TestTable.put(index, a);
+							a.v4 = (int)index1;
+							AllTables.TestTable.put(index1, a);
 						}
 						if(b == null)
 						{
 							b = new TestType();
-							b.v4 = n - (int)index - 1;
-							AllTables.TestTable.put(n - index - 1, b);
+							b.v4 = (int)index2;
+							AllTables.TestTable.put(index2, b);
 						}
 						a.v4 += b.v4;
-						AllTables.TestTable.modify(index, a);
+						AllTables.TestTable.modify(index1, a);
 						tt = System.currentTimeMillis() - t1;
 						if(tt >= 250) System.out.println("--- proc timeout=" + tt);
 						return true;
--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 cd `dirname $0`
 
-JVM="-Xms512m -Xmx512m -server -XX:+UseConcMarkSweepGC"
+JVM="-Xms512m -Xmx512m -server -XX:+UseConcMarkSweepGC -Xloggc:log/gc.log -XX:+PrintGCTimeStamps"
 LIB="lib/slf4j-api-1.7.5.jar:lib/logback-core-1.0.13.jar:lib/logback-classic-1.0.13.jar:lib/mina-core-2.0.7.jar:lib/luaj-jse-2.0.3.jar"
 
 if [ "$1" == "" ] ; then set $1 sas.test.TestMain; fi
