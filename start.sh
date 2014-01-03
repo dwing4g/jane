92cdb605e7bf14dbf41784378326d3120e13316e
dwing <dwing4g@gmail.com>
2014-01-03 19:16:01 +0800

    fix start.sh

--- start.sh
+++ start.sh
@@ -5,10 +5,11 @@ cd `dirname $0`
 JVM="-Xms512m -Xmx512m -server -XX:+UseConcMarkSweepGC -Xloggc:log/gc.log -XX:+PrintGCTimeStamps"
 LIB="lib/slf4j-api-1.7.5.jar:lib/logback-core-1.0.13.jar:lib/logback-classic-1.0.13.jar:lib/mina-core-2.0.7.jar:lib/luaj-jse-2.0.3.jar"
 
-if [ "$1" == "" ] ; then set $1 jane.test.TestMain; fi
-if [ "$1" == "b" ] ; then set $1 jane.test.TestDBBenchmark; fi
+MAIN=$1
+if [ "$MAIN" == "" ]; then MAIN=jane.test.TestMain; fi
+if [ "$MAIN" == "b" ]; then MAIN=jane.test.TestDBBenchmark; fi
 
 mkdir -p log 2> /dev/null
 mkdir -p db 2> /dev/null
 
-java $JVM -cp $LIB:jane-core.jar:jane-test.jar:. $@
+java $JVM -cp $LIB:jane-core.jar:jane-test.jar:. $MAIN ${@:2:9}
