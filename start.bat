038ffbf5cf42f7e9810c9deb06f78867ac7c645b
dwing <dwing4g@gmail.com>
2014-02-27 16:43:43 +0800

    update mapdb & mvstore & slf4j

--- start.bat
+++ start.bat
@@ -3,13 +3,13 @@ setlocal
 pushd %~dp0
 
 set JVM=-Xms512m -Xmx512m -server -XX:+UseConcMarkSweepGC -Xloggc:log/gc.log -XX:+PrintGCTimeStamps
-set LIB=lib/slf4j-api-1.7.5.jar;lib/logback-core-1.1.1.jar;lib/logback-classic-1.1.1.jar;lib/mina-core-2.0.7.jar;lib/luaj-jse-2.0.3.jar
+set LIB=lib/slf4j-api-1.7.6.jar;lib/logback-core-1.1.1.jar;lib/logback-classic-1.1.1.jar;lib/mina-core-2.0.7.jar;lib/luaj-jse-2.0.3.jar
 
 set MAIN=%1
-if "%MAIN%" equ "" set MAIN=jane.test.TestMain
+if "%MAIN%" equ ""  set MAIN=jane.test.TestMain
 if "%MAIN%" equ "b" set MAIN=jane.test.TestDBBenchmark
 
 md log 2>nul
-md db 2>nul
+md db  2>nul
 
 java %JVM% -cp %LIB%;jane-core.jar;jane-test.jar;. %MAIN% %2 %3 %4 %5 %6 %7 %8 %9
--- start.sh
+++ start.sh
@@ -3,13 +3,15 @@
 cd `dirname $0`
 
 JVM="-Xms512m -Xmx512m -server -XX:+UseConcMarkSweepGC -Xloggc:log/gc.log -XX:+PrintGCTimeStamps"
-LIB="lib/slf4j-api-1.7.5.jar:lib/logback-core-1.1.1.jar:lib/logback-classic-1.1.1.jar:lib/mina-core-2.0.7.jar:lib/luaj-jse-2.0.3.jar"
+LIB="lib/slf4j-api-1.7.6.jar:lib/logback-core-1.1.1.jar:lib/logback-classic-1.1.1.jar:lib/mina-core-2.0.7.jar:lib/luaj-jse-2.0.3.jar"
 
 MAIN=$1
-if [ "$MAIN" == "" ]; then MAIN=jane.test.TestMain; fi
-if [ "$MAIN" == "b" ]; then MAIN=jane.test.TestDBBenchmark; fi
+if [ "$MAIN" == "nohup" ]; then NOHUP="nohup"; MAIN=$2; fi
+if [ "$MAIN" == ""      ]; then MAIN=jane.test.TestMain; fi
+if [ "$MAIN" == "b"     ]; then MAIN=jane.test.TestDBBenchmark; fi
 
 mkdir -p log 2> /dev/null
-mkdir -p db 2> /dev/null
+mkdir -p db  2> /dev/null
 
-java $JVM -cp $LIB:jane-core.jar:jane-test.jar:. $MAIN ${@:2:9}
+if [ "$NOHUP" == "" ]; then java $JVM -cp $LIB:jane-core.jar:jane-test.jar:. $MAIN ${@:2:9}
+else                  nohup java $JVM -cp $LIB:jane-core.jar:jane-test.jar:. $MAIN ${@:3:9} 1>> log/stdout.log 2>> log/stderr.log & fi
