443a4860f2c574ca3758f19ec12d4be5ebd5e7a3
dwing <dwing4g@gmail.com>
2014-03-13 18:01:09 +0800

    some fixes

--- src/jane/test/TestLuaClient.lua
+++ src/jane/test/TestLuaClient.lua
@@ -1,4 +1,3 @@
-local conf_allbeans = "jane.bean.AllBeans"
 local conf_handler = "TestClient"
 local conf_addr = "127.0.0.1"
 local conf_port = 9123
@@ -6,11 +5,13 @@ local conf_port = 9123
 local luajava = luajava
 local class = luajava.bindClass
 local new = luajava.newInstance
+
 print("welcome to client console in lua")
 log = class("jane.core.Log").log
-local allbeans = class(conf_allbeans)
-allbeans:register()
-local client = new("jane.core.BeanManager")
+local allbeans = class("jane.bean.AllBeans")
+class("jane.core.BeanCodec"):registerAllBeans(allbeans:getAllBeans())
+
+local client = new("jane.core.NetManager")
 -- client:setHandlers(allbeans["get" .. conf_handler .. "Handlers"](allbeans))
 client:startClient(new("java.net.InetSocketAddress", conf_addr, conf_port))
 function send(name, ...)
