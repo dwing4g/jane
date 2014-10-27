85bbb53a319ab328110ee0ad5c625e33819a2664
dwing <dwing4g@gmail.com>
2014-10-27 10:30:32 +0800

    update mina from 2.0.8 to 2.0.9

--- start.bat
+++ start.bat
@@ -17,7 +17,7 @@ lib/slf4j-api-1.7.7.jar;^
 lib/log4j-core-2.1.jar;^
 lib/log4j-api-2.1.jar;^
 lib/log4j-slf4j-impl-2.1.jar;^
-lib/mina-core-2.0.8.jar;^
+lib/mina-core-2.0.9.jar;^
 lib/luaj-jse-2.0.3.jar;^
 lib/h2mvstore-1.4.182.jar;^
 jane-core.jar;^
