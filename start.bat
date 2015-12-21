06bcb904116207ac5872161c0e37dad36ced2757
dwing <dwing4g@gmail.com>
2015-12-21 18:02:40 +0800

    update mina(2.0.9 => 2.0.10) and slf4j(1.7.12 => 1.7.13)

--- start.bat
+++ start.bat
@@ -13,11 +13,11 @@ set JVM=^
 -verbose:gc
 
 set LIB=^
-lib/slf4j-api-1.7.12.jar;^
+lib/slf4j-api-1.7.13.jar;^
 lib/log4j-core-2.3.jar;^
 lib/log4j-api-2.3.jar;^
 lib/log4j-slf4j-impl-2.3.jar;^
-lib/mina-core-2.0.9.jar;^
+lib/mina-core-2.0.10.jar;^
 lib/luaj-jse-2.0.3.jar;^
 lib/h2-mvstore-1.4.188.jar;^
 jane-core.jar;^
