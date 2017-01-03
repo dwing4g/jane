c4b65454d6d2f2fd6a3a700fac0d7eb43561423e
dwing <dwing4g@gmail.com>
2017-01-03 15:48:56 +0800

    update doc; update luajit

--- doc/build_mvstore.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-setlocal
-pushd %~dp0
-
-build.bat jarMVStore
-rem luajit build_mvstore_src.lua
-
-pause
--- tool/update_lib.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal
-pushd %~dp0
-
-set MAPDB=..\..\mapdb
-
-copy /y %MAPDB%\src\main\java\org\mapdb\*.java ..\lib\mapdb\org\mapdb\
-
-pause
