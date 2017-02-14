5fe03acd13f31da15a430bf0e8f123a1b75cd5b7
dwing <dwing4g@gmail.com>
2017-02-14 17:20:32 +0800

    add experimental simple network core based on NIO.2

---
+++ src/jane/test/net/TcpManager.java
@@ -0,0 +1,115 @@
+package jane.test.net;
+
+import java.io.IOException;
+import java.net.SocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.AsynchronousServerSocketChannel;
+import java.nio.channels.AsynchronousSocketChannel;
+import java.nio.channels.CompletionHandler;
+
+public class TcpManager
+{
+	private AsynchronousServerSocketChannel	_acceptor;
+	private final AcceptHandler				_acceptHandler	= new AcceptHandler();
+	private final ConnectHandler			_connectHandler	= new ConnectHandler();
+
+	public synchronized void startServer(SocketAddress addr) throws IOException
+	{
+		if(_acceptor == null)
+			_acceptor = AsynchronousServerSocketChannel.open();
+		_acceptor.bind(addr);
+		_acceptor.accept(this, _acceptHandler);
+	}
+
+	public synchronized void stopServer() throws IOException
+	{
+		if(_acceptor != null)
+		{
+			_acceptor.close();
+			_acceptor = null;
+		}
+	}
+
+	public void startClient(SocketAddress addr) throws IOException
+	{
+		@SuppressWarnings("resource")
+		AsynchronousSocketChannel connector = AsynchronousSocketChannel.open();
+		connector.connect(addr, connector, _connectHandler);
+	}
+
+	@SuppressWarnings("unused")
+	public void onAddSession(TcpSession session)
+	{
+	}
+
+	@SuppressWarnings("unused")
+	public void onDelSession(TcpSession session)
+	{
+	}
+
+	@SuppressWarnings("unused")
+	public void onReceive(TcpSession session, ByteBuffer bb, int size)
+	{
+	}
+
+	@SuppressWarnings("unused")
+	public void onConnectFailed(SocketAddress addr, Throwable ex)
+	{
+	}
+
+	@SuppressWarnings("unused")
+	public void onException(TcpSession session, Throwable ex)
+	{
+	}
+
+	private final class AcceptHandler implements CompletionHandler<AsynchronousSocketChannel, Object>
+	{
+		@Override
+		public void completed(AsynchronousSocketChannel channel, Object attachment)
+		{
+			try
+			{
+				onAddSession(new TcpSession(TcpManager.this, channel));
+			}
+			catch(Throwable e)
+			{
+				onException(null, e);
+			}
+		}
+
+		@Override
+		public void failed(Throwable ex, Object attachment)
+		{
+			onException(null, ex);
+		}
+	}
+
+	private final class ConnectHandler implements CompletionHandler<Void, Object>
+	{
+		@Override
+		public void completed(Void result, Object attachment)
+		{
+			try
+			{
+				onAddSession(new TcpSession(TcpManager.this, (AsynchronousSocketChannel)attachment));
+			}
+			catch(Throwable e)
+			{
+				onException(null, e);
+			}
+		}
+
+		@Override
+		public void failed(Throwable ex, Object attachment)
+		{
+			try
+			{
+				onConnectFailed(((AsynchronousSocketChannel)attachment).getRemoteAddress(), ex);
+			}
+			catch(IOException e)
+			{
+				onException(null, e);
+			}
+		}
+	}
+}
---
+++ src/jane/test/net/TcpSession.java
@@ -0,0 +1,146 @@
+package jane.test.net;
+
+import java.nio.ByteBuffer;
+import java.nio.channels.AsynchronousSocketChannel;
+import java.nio.channels.CompletionHandler;
+import java.util.ArrayDeque;
+
+public final class TcpSession
+{
+	public static final int	RECV_BUF_SIZE	 = 8192;
+	public static final int	SEND_BUF_MAXSIZE = 1024576;
+
+	private final TcpManager				_manager;
+	private final AsynchronousSocketChannel	_channel;
+	private final ByteBuffer				_recvBuf;
+	private final ArrayDeque<ByteBuffer>	_sendBuf	 = new ArrayDeque<>();
+	private final RecvHandler				_recvHandler = new RecvHandler();
+	private final SendHandler				_sendHandler = new SendHandler();
+	private int								_sendBufSize;
+
+	TcpSession(TcpManager manager, AsynchronousSocketChannel channel)
+	{
+		_manager = manager;
+		_channel = channel;
+		_recvBuf = ByteBuffer.allocateDirect(RECV_BUF_SIZE);
+		try
+		{
+			channel.read(_recvBuf, null, _recvHandler);
+		}
+		catch(Throwable e)
+		{
+			manager.onException(this, e);
+		}
+	}
+
+	public int getSendQueueSize()
+	{
+		synchronized(_sendBuf)
+		{
+			return _sendBuf.size();
+		}
+	}
+
+	public int getSendBufSize()
+	{
+		return _sendBufSize;
+	}
+
+	public void send(ByteBuffer bb)
+	{
+		int size = bb.remaining();
+		boolean isEmpty;
+		synchronized(_sendBuf)
+		{
+			int newSize = _sendBufSize + size;
+			if(newSize > SEND_BUF_MAXSIZE || newSize < 0)
+			{
+				_manager.onException(TcpSession.this, new Exception(String.format("send overflow(%d=>%d)", size, _sendBufSize)));
+				return;
+			}
+			_sendBufSize = newSize;
+			isEmpty = _sendBuf.isEmpty();
+			_sendBuf.addLast(bb);
+		}
+		if(isEmpty)
+			_channel.write(bb, null, _sendHandler);
+	}
+
+	private final class RecvHandler implements CompletionHandler<Integer, Object>
+	{
+		@Override
+		public void completed(Integer result, Object attachment)
+		{
+			int size = result.intValue();
+			if(size > 0)
+			{
+				try
+				{
+					_manager.onReceive(TcpSession.this, _recvBuf, size);
+				}
+				catch(Throwable e)
+				{
+					_manager.onException(TcpSession.this, e);
+				}
+				try
+				{
+					_channel.read(_recvBuf, null, this);
+				}
+				catch(Throwable e)
+				{
+					_manager.onException(TcpSession.this, e);
+				}
+			}
+			else
+			{
+				try
+				{
+					_manager.onDelSession(TcpSession.this);
+				}
+				catch(Throwable e)
+				{
+					_manager.onException(TcpSession.this, e);
+				}
+			}
+		}
+
+		@Override
+		public void failed(Throwable ex, Object attachment)
+		{
+			_manager.onException(TcpSession.this, ex);
+		}
+	}
+
+	private final class SendHandler implements CompletionHandler<Integer, Object>
+	{
+		@Override
+		public void completed(Integer result, Object attachment)
+		{
+			int size = result.intValue();
+			ByteBuffer bb;
+			synchronized(_sendBuf)
+			{
+				bb = _sendBuf.pollFirst();
+				int left = bb.remaining();
+				if(size < left)
+				{
+					bb.position(bb.position() + size);
+					_sendBufSize -= size;
+				}
+				else
+				{
+					bb = _sendBuf.peekFirst();
+					_sendBufSize -= left;
+				}
+			}
+			if(bb != null)
+				_channel.write(bb, null, _sendHandler);
+		}
+
+		@Override
+		public void failed(Throwable ex, Object attachment)
+		{
+			_manager.onException(TcpSession.this, ex);
+		}
+	}
+}
