86217c44fe4928c11a7494971f91306ec9fa34e8
dwing <dwing4g@gmail.com>
2017-10-19 18:40:18 +0800

    add some comment for Libuv

--- src/jane/test/net/Libuv.java
+++ src/jane/test/net/Libuv.java
@@ -4,6 +4,13 @@ import java.nio.ByteBuffer;
 
 /**
  * 基于libuv本地库的TCP网络处理
+ * <p>
+ * libuv的特点是单线程的事件处理循环,异步无阻塞,事件响应式,极其优化的底层IO实现(使用epoll,kqueue,IOCP),极少的内存开销<br>
+ * 由于一个处理循环只绑定到一个线程,一个端口监听到的所有连接只能绑定监听所在的循环,所以一个端口的大规模连接可能会是瓶颈,即使底层IO效率极高<br>
+ * 如果仍然需要利用libuv,可以考虑把复杂耗时的回调处理移到其它线程执行(会带来线程间通信开销和延迟),或者分多端口监听,每个端口使用不同的处理循环和线程<br>
+ * 由于几乎没有内存分配开销,接收数据零拷贝,所以接收的数据需要即时处理或复制出来,另外为了保持接口简单,发送数据接口也要排队调用,成功回调前不能多次发送<br>
+ * 通过单纯的单线程echo的性能测试表明libuv性能是mina,aio,netty等使用NIO/AIO的1.5倍,几乎没有内存分配开销<br>
+ * 需要加载本地库(https://github.com/dwing4g/libuv): uvjni{32|64}.dll,libuvjni{32|64}.so<br>
  */
 public final class Libuv
 {
@@ -61,7 +68,7 @@ public final class Libuv
 	/**
 	 * 销毁一个libuv的处理循环(uv_loop_close)
 	 * @param handle_loop 处理循环的句柄
-	 * @return 0表示正常销毁
+	 * @return 0表示正常销毁. 如果该循环还可能继续接收事件处理,则返回非0表示不能销毁成功
 	 */
 	public static native int libuv_loop_destroy(long handle_loop);
 
@@ -87,7 +94,7 @@ public final class Libuv
 	 * @param ip 本地监听的IP. null或"0.0.0.0"表示全部的本地IP
 	 * @param port 本地监听的端口
 	 * @param backlog 等待被动连接的队列最大数量
-	 * @return 0表示绑定成功,后续会回调onOpen来通知新连接
+	 * @return 0表示绑定成功,后续会回调onOpen来通知成功的新连接(accept回调处理的一些异常情况目前被底层忽略而没有途径响应到handler中)
 	 */
 	public static native int libuv_tcp_bind(long handle_loop, String ip, int port, int backlog);
 
@@ -97,7 +104,7 @@ public final class Libuv
 	 * @param handle_loop 处理循环的句柄
 	 * @param ip 主动连接的远程IP
 	 * @param port 主动连接的远程端口
-	 * @return 0表示连接初步成功,最终结果等回调onOpen
+	 * @return 0表示连接初步成功,最终结果等回调onOpen(成功时)或onClose(失败时)
 	 */
 	public static native int libuv_tcp_connect(long handle_loop, String ip, int port);
 
