f41583bc84dc19cc74208ca53c60a1d343d0277b
dwing <dwing4g@gmail.com>
2017-12-23 12:50:44 +0800

    add mac os x support

--- genbeans.sh
+++ genbeans.sh
@@ -2,6 +2,12 @@
 
 cd `dirname $0`
 
+if [ `uname -s` == Darwin ]; then
+	LUAJIT=tool/luajit_mac
+else
+	LUAJIT=tool/luajit
+fi
+
 # LIB=lib/luaj-jse-2.0.3.jar
 
 mkdir -p src/jane/bean 2> /dev/null
@@ -12,12 +18,12 @@ mkdir -p port/csharp/Jane/Bean 2> /dev/null
 mkdir -p port/csharp/Jane/Handler 2> /dev/null
 # mkdir -p port/csharp/Jane/Handler/... 2> /dev/null
 
-tool/luajit tool/format_allbeans.lua allbeans.lua
+$LUAJIT tool/format_allbeans.lua allbeans.lua
 
 # java -cp $LIB lua tool/genbeans.lua jane.bean Server,Client allbeans.lua src
 # java -cp $LIB lua tool/genbeans_csharp.lua Jane.Bean ClientCS allbeans.lua port/csharp
 # java -cp $LIB lua tool/genbeans_lua.lua Client allbeans.lua port/lua/src
 
-tool/luajit tool/genbeans.lua jane.bean Server,Client allbeans.lua src
-tool/luajit tool/genbeans_csharp.lua Jane.Bean ClientCS allbeans.lua port/csharp
-tool/luajit tool/genbeans_lua.lua Client allbeans.lua port/lua/src
+$LUAJIT tool/genbeans.lua jane.bean Server,Client allbeans.lua src
+$LUAJIT tool/genbeans_csharp.lua Jane.Bean ClientCS allbeans.lua port/csharp
+$LUAJIT tool/genbeans_lua.lua Client allbeans.lua port/lua/src
