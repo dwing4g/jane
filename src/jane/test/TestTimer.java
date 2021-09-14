8d80e43db4ad3a2cc750d499a7324bb952977e8d
dwing <dwing4g@gmail.com>
2021-09-14 18:09:22 +0800

    test for TestTimer

--- src/jane/test/TestTimer.java
+++ src/jane/test/TestTimer.java
@@ -28,7 +28,9 @@ public class TestTimer
 			}
 		}
 
-		public abstract void onTimer();
+		public void onTimer()
+		{
+		}
 	}
 
 	private static final int SLOT_SHIFT	 = 8;
@@ -37,23 +39,28 @@ public class TestTimer
 	private static final int SLOT_SIZE	 = (1 << SLOT_SHIFT) * SLOTS_COUNT;
 
 	private final Node[] nodeHeads = new Node[SLOT_SIZE + 1];
-	private long		 time;
+	private long		 curTime;
 
 	public TestTimer(long curTime)
 	{
-		time = curTime;
+		this.curTime = curTime;
 	}
 
-	public void add(Node node, long runTime)
+	public long curTime()
+	{
+		return curTime;
+	}
+
+	public void add(long runTime, Node node)
 	{
 		if (node.next != null)
-			throw new IllegalStateException("the node is already in timer");
+			throw new IllegalStateException("added already");
 		final int idx;
-		final long dt = runTime - time;
+		final long dt = runTime - curTime;
 		if (dt < (1L << SLOT_SHIFT))
 		{
 			if (dt < 0)
-				idx = runTime < time ? (int)(runTime = time) & SLOT_MASK : SLOT_SIZE;
+				idx = runTime < curTime ? (int)(runTime = curTime) & SLOT_MASK : SLOT_SIZE;
 			else
 				idx = (int)runTime & SLOT_MASK;
 		}
@@ -65,7 +72,7 @@ public class TestTimer
 			idx = (3 << SLOT_SHIFT) + ((int)(runTime >>> (SLOT_SHIFT * 3)) & SLOT_MASK);
 		else if (dt < (1L << (SLOT_SHIFT * 5)))
 			idx = (4 << SLOT_SHIFT) + ((int)(runTime >>> (SLOT_SHIFT * 4)) & SLOT_MASK);
-		else // overflow
+		else // SLOTS_COUNT overflow
 			idx = SLOT_SIZE;
 		final Node[] heads = nodeHeads;
 		final Node head = heads[idx];
@@ -82,57 +89,107 @@ public class TestTimer
 
 	public void update(long curTime)
 	{
-		long t = time;
+		long t = this.curTime;
 		if (curTime < t)
 			return;
-		for (final Node[] heads = nodeHeads;;)
+		final Node[] heads = nodeHeads;
+		for (int idx = (int)t & SLOT_MASK;;)
 		{
-			int idx = (int)t & SLOT_MASK;
-			for (Node head; (head = heads[idx]) != null;)
+			Node head = heads[idx];
+			if (head != null)
 			{
-				heads[idx] = null;
-				for (Node node = head.next;;)
-				{
-					final Node next = node.next;
-					node.next = null;
-					node.doTimer();
-					if (node == head)
-						break;
-					node = next;
-				}
-			}
-			if (curTime == t)
-				return;
-			time = ++t;
-			long tt = t;
-			for (int i = 1; (tt & SLOT_MASK) == 0 && i < SLOTS_COUNT; i++)
-			{
-				idx = (i << SLOT_SHIFT) + ((int)(tt >>>= SLOT_SHIFT) & SLOT_MASK);
-				final Node head = heads[idx];
-				if (head != null)
+				this.curTime = t;
+				do
 				{
 					heads[idx] = null;
-					final int base = (i - 1) << SLOT_SHIFT;
-					final int shift = (i - 1) * SLOT_SHIFT;
 					for (Node node = head.next;;)
 					{
 						final Node next = node.next;
-						idx = base + (int)(node.runTime >>> shift) & SLOT_MASK;
-						final Node head0 = heads[idx];
-						if (head0 == null)
-							node.next = node;
-						else
-						{
-							node.next = head0.next;
-							head0.next = node;
-						}
-						heads[idx] = node;
+						node.next = null;
+						node.doTimer();
 						if (node == head)
 							break;
 						node = next;
 					}
 				}
+				while ((head = heads[idx]) != null);
+			}
+			if (t == curTime)
+			{
+				this.curTime = t;
+				return;
+			}
+			if ((idx = (int)++t & SLOT_MASK) == 0)
+			{
+				int i = 1;
+				while (((t >>> (i * SLOT_SHIFT)) & SLOT_MASK) == 0 && i < SLOTS_COUNT - 1)
+					i++;
+				for (; i > 0; i--)
+				{
+					int base = i << SLOT_SHIFT;
+					int shift = i * SLOT_SHIFT;
+					int idx1 = base + ((int)(t >>> shift) & SLOT_MASK);
+					if ((head = heads[idx1]) != null)
+					{
+						heads[idx1] = null;
+						base -= 1 << SLOT_SHIFT;
+						shift -= SLOT_SHIFT;
+						for (Node node = head.next;;)
+						{
+							final Node next = node.next;
+							idx1 = base + ((int)(node.runTime >>> shift) & SLOT_MASK);
+							final Node head1 = heads[idx1];
+							if (head1 == null)
+								node.next = node;
+							else
+							{
+								node.next = head1.next;
+								head1.next = node;
+							}
+							heads[idx1] = node;
+							if (node == head)
+								break;
+							node = next;
+						}
+					}
+				}
 			}
 		}
+	}
+
+	public static void main(String[] args)
+	{
+		final long t = System.nanoTime();
+		final var state = new Object()
+		{
+			long runCount;
+			long seed = t;
+
+			long rand()
+			{
+				return seed = seed * 6364136223846793005L + 1442695040888963407L;
+			}
+		};
+		final TestTimer timer = new TestTimer((int)state.rand());
+		long curTime = timer.curTime();
+		for (int i = 0; i < 1_000; i++)
+		{
+			timer.add(curTime + (state.rand() & 0x3ff), new Node()
+			{
+				@Override
+				public void onTimer()
+				{
+					final long runTime = runTime();
+					final long curTime = timer.curTime();
+					if (runTime != curTime)
+						throw new AssertionError(String.format("0x%X != 0x%X", runTime, curTime));
+					state.runCount++;
+					timer.add(runTime + (state.rand() & 0x1_ffff), this);
+				}
+			});
+		}
+		for (int i = 0; i < 100_000_000; i++)
+			timer.update(curTime += 33);
+		System.out.println(state.runCount + ", " + (System.nanoTime() - t) / 1_000_000 + " ms");
 	}
 }
